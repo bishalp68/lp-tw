9eb046922b6ba54823a1b59a8b7dedfa42ff6e19
bishalp68 <bpakhrin68@gmail.com>
2025-08-01 18:32:12 +0900

    inital setup
    Added node and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                space: ['Space Grotesk', 'sans-serif'],
+            },
+            colors: {
+                primary: '#B9FF66',
+                secondary: '#191A23',
+                dark: {
+                    100: '#1f2937',
+                    200: '#111827',
+                },
+            },
+        },
+    },
+    plugins: [],
+};
