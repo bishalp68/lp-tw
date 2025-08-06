65a51a76a3d0b3efee30b1b365fd7b0dbd2204da
bishalp68 <bpakhrin68@gmail.com>
2025-08-06 19:42:38 +0900

    completed few blocks
    Complated cta,study and progress block in progress

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 $(document).ready(() => {
-    $('.slick').slick({
-        slidesToShow: 2,
+    $('.logo-slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 2000,
+        cssEase: 'linear',
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
     });
+
+    $('.cta-slider').slick({
+        slidesToShow: 3,
+    });
+
+    //accordion
+    $(".accordion").on("click", ".accordion-header", function () {
+
+        $(this).parent().toggleClass("active");
+        $(this).toggleClass("active").next().slideToggle();
+
+        $(".accordion-content").not($(this).next()).slideUp(300);
+
+        $(this).siblings().removeClass("active");
+    });
+
 });
